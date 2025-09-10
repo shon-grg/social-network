a6908c68362246f45471a12ecda2c9cbf2759108
Shon George <shongeorge2000@gmail.com>
2025-09-10 21:34:27 +0530

    route create

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect database
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+// Define routes
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 3000;
 
